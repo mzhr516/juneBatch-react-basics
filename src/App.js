a9b9c1e2806e8cb747cf0f90fe2cd69d053f1180
mohdmazharuddin <106807606+mohdmazharuddin@users.noreply.github.com>
2023-06-13 15:37:52 +0530

    add carssol component and react fragment

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import Carasol from "./componets/Carasol";
 import Body from "./componets/Body";
 import Footer from "./componets/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
+import BootstrapCarasol from "./componets/BootstrapCarasol";
+import Greeting from "./componets/Greeting";
 
 function App() {
   return (
     <div className="mainContainer">
       <NavBar />
       <Carasol />
+      <BootstrapCarasol />
       <Body />
+      <div className="greeting">
+        <Greeting />
+      </div>
       {Footer()}
       <h1 className="h1 border border-dark">good evening....</h1>
     </div>
---
+++ src/componets/Greeting.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+const Greeting = () => {
+  return (
+    <Fragment>
+      <h1>hello good morning</h1>
+      <p>have a nice day</p>
+    </Fragment>
+  );
+};
+
+export default Greeting;

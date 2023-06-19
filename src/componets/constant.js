7a1900a245966c1846a2b32c858cad96a6b85336
mohdmazharuddin <106807606+mohdmazharuddin@users.noreply.github.com>
2023-06-19 13:41:51 +0530

    added more arrays

--- src/componets/constant.js
+++ src/componets/constant.js
@@ -116,195 +116,278 @@ export const menus = [
 ];
 
 export const carDetails = [
+  {
+    make: "Toyota",
+    model: "Camry",
+    year: 2020,
+    color: "Silver",
+    mileage: 25000,
+    fuelType: "Gasoline",
+    transmission: "Automatic",
+    price: 25000,
+  },
+  {
+    make: "Honda",
+    model: "Civic",
+    year: 2018,
+    color: "Black",
+    mileage: 40000,
+    fuelType: "Gasoline",
+    transmission: "Automatic",
+    price: 18000,
+  },
+  {
+    make: "Ford",
+    model: "Mustang",
+    year: 2022,
+    color: "Red",
+    mileage: 1000,
+    fuelType: "Gasoline",
+    transmission: "Manual",
+    price: 40000,
+  },
+  {
+    make: "Chevrolet",
+    model: "Malibu",
+    year: 2019,
+    color: "White",
+    mileage: 32000,
+    fuelType: "Gasoline",
+    transmission: "Automatic",
+    price: 22000,
+  },
+  {
+    make: "BMW",
+    model: "X5",
+    year: 2021,
+    color: "Blue",
+    mileage: 15000,
+    fuelType: "Diesel",
+    transmission: "Automatic",
+    price: 50000,
+  },
+  {
+    make: "Mercedes-Benz",
+    model: "E-Class",
+    year: 2019,
+    color: "Silver",
+    mileage: 28000,
+    fuelType: "Gasoline",
+    transmission: "Automatic",
+    price: 35000,
+  },
+  {
+    make: "Audi",
+    model: "A4",
+    year: 2020,
+    color: "Gray",
+    mileage: 20000,
+    fuelType: "Gasoline",
+    transmission: "Automatic",
+    price: 30000,
+  },
+  {
+    make: "Lexus",
+    model: "RX",
+    year: 2022,
+    color: "White",
+    mileage: 5000,
+    fuelType: "Hybrid",
+    transmission: "Automatic",
+    price: 45000,
+  },
+  {
+    make: "Volkswagen",
+    model: "Golf",
+    year: 2017,
+    color: "Red",
+    mileage: 55000,
+    fuelType: "Gasoline",
+    transmission: "Automatic",
+    price: 15000,
+  },
+  {
+    make: "Tesla",
+    model: "Model 3",
+    year: 2023,
+    color: "White",
+    mileage: 100,
+    fuelType: "Electric",
+    transmission: "Automatic",
+    price: 60000,
+  },
+  {
+    make: "Subaru",
+    model: "Outback",
+    year: 2020,
+    color: "Green",
+    mileage: 18000,
+    fuelType: "Gasoline",
+    transmission: "CVT",
+    price: 28000,
+  },
+  {
+    make: "Jeep",
+    model: "Wrangler",
+    year: 2021,
+    color: "Black",
+    mileage: 8000,
+    fuelType: "Gasoline",
+    transmission: "Manual",
+    price: 35000,
+  },
+  {
+    make: "Hyundai",
+    model: "Tucson",
+    year: 2019,
+    color: "Silver",
+    mileage: 25000,
+    fuelType: "Gasoline",
+    transmission: "Automatic",
+    price: 20000,
+  },
+  {
+    make: "Mazda",
+    model: "CX-5",
+    year: 2022,
+    color: "Red",
+    mileage: 5000,
+    fuelType: "Gasoline",
+    transmission: "Automatic",
+    price: 32000,
+  },
+  {
+    make: "Kia",
+    model: "Sportage",
+    year: 2018,
+    color: "Blue",
+    mileage: 35000,
+    fuelType: "Gasoline",
+    transmission: "Automatic",
+    price: 18000,
+  },
+  {
+    make: "Nissan",
+    model: "Altima",
+    year: 2020,
+    color: "Silver",
+    mileage: 30000,
+    fuelType: "Gasoline",
+    transmission: "Automatic",
+    price: 22000,
+  },
+  {
+    make: "Volvo",
+    model: "XC90",
+    year: 2021,
+    color: "Black",
+    mileage: 12000,
+    fuelType: "Gasoline",
+    transmission: "Automatic",
+    price: 55000,
+  },
+  {
+    make: "Land Rover",
+    model: "Range Rover Evoque",
+    year: 2019,
+    color: "White",
+    mileage: 24000,
+    fuelType: "Gasoline",
+    transmission: "Automatic",
+    price: 40000,
+  },
+  {
+    make: "Porsche",
+    model: "911",
+    year: 2022,
+    color: "Yellow",
+    mileage: 5000,
+    fuelType: "Gasoline",
+    transmission: "Automatic",
+    price: 100000,
+  },
+];
+
+const cityDetails = [
     {
-      make: "Toyota",
-      model: "Camry",
-      year: 2020,
-      color: "Silver",
-      mileage: 25000,
-      fuelType: "Gasoline",
-      transmission: "Automatic",
-      price: 25000
+      name: "New York City",
+      description: "The bustling metropolis known for its iconic landmarks, such as Times Square, Statue of Liberty, and Central Park."
     },
     {
-      make: "Honda",
-      model: "Civic",
-      year: 2018,
-      color: "Black",
-      mileage: 40000,
-      fuelType: "Gasoline",
-      transmission: "Automatic",
-      price: 18000
+      name: "Paris",
+      description: "The romantic capital of France famous for its enchanting architecture, Eiffel Tower, Louvre Museum, and delicious cuisine."
     },
     {
-      make: "Ford",
-      model: "Mustang",
-      year: 2022,
-      color: "Red",
-      mileage: 1000,
-      fuelType: "Gasoline",
-      transmission: "Manual",
-      price: 40000
+      name: "Tokyo",
+      description: "The vibrant capital of Japan blending tradition and modernity, featuring neon-lit streets, ancient temples, and world-class technology."
     },
     {
-      make: "Chevrolet",
-      model: "Malibu",
-      year: 2019,
-      color: "White",
-      mileage: 32000,
-      fuelType: "Gasoline",
-      transmission: "Automatic",
-      price: 22000
+      name: "London",
+      description: "The historic city in England, home to Buckingham Palace, Big Ben, Tower Bridge, and renowned museums like the British Museum."
     },
     {
-      make: "BMW",
-      model: "X5",
-      year: 2021,
-      color: "Blue",
-      mileage: 15000,
-      fuelType: "Diesel",
-      transmission: "Automatic",
-      price: 50000
+      name: "Rome",
+      description: "The eternal city in Italy with ancient ruins like the Colosseum, Roman Forum, Vatican City, and mouthwatering Italian food."
     },
     {
-      make: "Mercedes-Benz",
-      model: "E-Class",
-      year: 2019,
-      color: "Silver",
-      mileage: 28000,
-      fuelType: "Gasoline",
-      transmission: "Automatic",
-      price: 35000
+      name: "Sydney",
+      description: "The stunning harbor city in Australia, famous for the Sydney Opera House, Sydney Harbour Bridge, and beautiful beaches."
     },
     {
-      make: "Audi",
-      model: "A4",
-      year: 2020,
-      color: "Gray",
-      mileage: 20000,
-      fuelType: "Gasoline",
-      transmission: "Automatic",
-      price: 30000
+      name: "Dubai",
+      description: "The futuristic city in the United Arab Emirates known for its lavish shopping malls, ultramodern architecture, and luxury resorts."
     },
     {
-      make: "Lexus",
-      model: "RX",
-      year: 2022,
-      color: "White",
-      mileage: 5000,
-      fuelType: "Hybrid",
-      transmission: "Automatic",
-      price: 45000
+      name: "Rio de Janeiro",
+      description: "The vibrant coastal city in Brazil, celebrated for its breathtaking beaches, Christ the Redeemer statue, and lively carnival."
     },
     {
-      make: "Volkswagen",
-      model: "Golf",
-      year: 2017,
-      color: "Red",
-      mileage: 55000,
-      fuelType: "Gasoline",
-      transmission: "Automatic",
-      price: 15000
+      name: "Cape Town",
+      description: "The captivating city in South Africa, offering stunning landscapes, Table Mountain, vibrant markets, and rich cultural heritage."
     },
     {
-      make: "Tesla",
-      model: "Model 3",
-      year: 2023,
-      color: "White",
-      mileage: 100,
-      fuelType: "Electric",
-      transmission: "Automatic",
-      price: 60000
+      name: "Amsterdam",
+      description: "The picturesque city in the Netherlands, famous for its charming canals, historic buildings, Anne Frank House, and bicycles everywhere."
     },
     {
-      make: "Subaru",
-      model: "Outback",
-      year: 2020,
-      color: "Green",
-      mileage: 18000,
-      fuelType: "Gasoline",
-      transmission: "CVT",
-      price: 28000
+      name: "Barcelona",
+      description: "The cosmopolitan city in Spain renowned for its distinctive architecture, including the famous Sagrada Familia, vibrant street life, and delicious tapas."
     },
     {
-      make: "Jeep",
-      model: "Wrangler",
-      year: 2021,
-      color: "Black",
-      mileage: 8000,
-      fuelType: "Gasoline",
-      transmission: "Manual",
-      price: 35000
+      name: "Mumbai",
+      description: "The bustling metropolis in India, featuring Bollywood, historical landmarks like the Gateway of India, and a diverse culinary scene."
     },
     {
-      make: "Hyundai",
-      model: "Tucson",
-      year: 2019,
-      color: "Silver",
-      mileage: 25000,
-      fuelType: "Gasoline",
-      transmission: "Automatic",
-      price: 20000
+      name: "Istanbul",
+      description: "The transcontinental city bridging Europe and Asia, showcasing magnificent architecture, such as the Hagia Sophia and Blue Mosque, and a vibrant bazaar culture."
     },
     {
-      make: "Mazda",
-      model: "CX-5",
-      year: 2022,
-      color: "Red",
-      mileage: 5000,
-      fuelType: "Gasoline",
-      transmission: "Automatic",
-      price: 32000
+      name: "Buenos Aires",
+      description: "The captivating capital of Argentina known for its tango music and dance, colorful neighborhoods like La Boca, and delicious Argentine cuisine."
     },
     {
-      make: "Kia",
-      model: "Sportage",
-      year: 2018,
-      color: "Blue",
-      mileage: 35000,
-      fuelType: "Gasoline",
-      transmission: "Automatic",
-      price: 18000
+      name: "Toronto",
+      description: "The multicultural city in Canada with iconic landmarks like the CN Tower, diverse neighborhoods, and a thriving arts and entertainment scene."
     },
     {
-      make: "Nissan",
-      model: "Altima",
-      year: 2020,
-      color: "Silver",
-      mileage: 30000,
-      fuelType: "Gasoline",
-      transmission: "Automatic",
-      price: 22000
+      name: "Cairo",
+      description: "The ancient city in Egypt housing the iconic Pyramids of Giza, Sphinx, and rich cultural heritage dating back thousands of years."
     },
     {
-      make: "Volvo",
-      model: "XC90",
-      year: 2021,
-      color: "Black",
-      mileage: 12000,
-      fuelType: "Gasoline",
-      transmission: "Automatic",
-      price: 55000
+      name: "Seoul",
+      description: "The dynamic capital of South Korea blending modern technology, traditional palaces like Gyeongbokgung, vibrant markets, and street food culture."
     },
     {
-      make: "Land Rover",
-      model: "Range Rover Evoque",
-      year: 2019,
-      color: "White",
-      mileage: 24000,
-      fuelType: "Gasoline",
-      transmission: "Automatic",
-      price: 40000
+      name: "Berlin",
+      description: "The vibrant capital of Germany, renowned for its history, art scene, Berlin Wall remnants, Brandenburg Gate, and lively nightlife."
     },
     {
-      make: "Porsche",
-      model: "911",
-      year: 2022,
-      color: "Yellow",
-      mileage: 5000,
-      fuelType: "Gasoline",
-      transmission: "Automatic",
-      price: 100000
+      name: "San Francisco",
+      description: "The hilly city in California, USA, famous for the Golden Gate Bridge, Alcatraz Island, cable cars, and its thriving tech industry."
+    },
+    {
+      name: "Moscow",
+      description: "The iconic capital of Russia featuring stunning architecture like the Red Square, Kremlin, St. Basil's Cathedral, and renowned cultural institutions."
     }
   ];
   
